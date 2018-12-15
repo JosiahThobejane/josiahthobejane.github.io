52f0c9ff79010b438a0da9f81444e38b1f18eba7
JosiahThobejane <josiahponso@gmail.com>
2018-12-15 16:01:10 +0200

    fix the contact me modal bug

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,44 +1,51 @@
-var contactMeModal = document.getElementById('contactMeModal');
-var sendEmailButton = document.getElementById('sendEmailButton');
+document.addEventListener('DOMContentLoaded', () => {
+    var contactMeModal = document.getElementById('contactMeModal');
+    var sendEmailButton = document.getElementById('sendEmailButton');
 
-//when the Say Hello button is clicked, a modal should apprear
-document.getElementById('sayHelloButton').addEventListener('click', function (){
-    contactMeModal.classList.toggle('is-active');
-});
+    //when the Say Hello button is clicked, a modal should apprear
+    document.getElementById('sayHelloButton').addEventListener('click', function (){
+        contactMeModal.classList.toggle('is-active');
+    });
 
-//when this is clicked, the modal should vanish into thin air
-document.getElementById('closeModal').addEventListener('click', function() {
-    contactMeModal.classList.toggle('modal-close');
-});
-jfff
-//when clicked, this should send an email
-sendEmailButton.addEventListener('click', function(){
-    sendEmailButton.classList.toggle('is-loading');
+    //when this is clicked, the modal should vanish into thin air
+    document.getElementById('closeModal').addEventListener('click', function() {
+        contactMeModal.classList.toggle('modal-close');
+    });
 
-    var personEmail = document.getElementById("personEmail").value;
-    var personName = document.getElementById("personName").value;
-    var personMessage = document.getElementById("personMessage").value;    
-    var myEmail = "josiahponso@gmail.com";
+    //when clicked, this should send an email
+    sendEmailButton.addEventListener('click', function(){
+        sendEmailButton.classList.toggle('is-loading');
 
-    var template_params = {
-        "reply_to": personEmail,
-        "from_name": personName,
-        "to_name": myEmail,
-        "message_html": personMessage
-     }
-     
-    var service_id = "default_service";
-    var template_id = "template_Ax2BBqG2";
+        var personEmail = document.getElementById("personEmail").value;
+        var personName = document.getElementById("personName").value;
+        var personMessage = document.getElementById("personMessage").value;    
+        var myEmail = "josiahponso@gmail.com";
 
-    //wait to seconds before executing the sendMessage() function
-    setTimeout(sendMessage, 2000);
+        var template_params = {
+            "reply_to": personEmail,
+            "from_name": personName,
+            "to_name": myEmail,
+            "message_html": personMessage
+        }
+        
+        var service_id = "default_service";
+        var template_id = "template_Ax2BBqG2";
 
-    function sendMessage() {
-        emailjs.send(service_id,template_id,template_params);
+        //wait to seconds before executing the sendMessage() function
+        setTimeout(sendMessage, 2000);
 
-        sendEmailButton.classList.remove('is-loading');        
-        sendEmailButton.classList.toggle('is-danger');
-        sendEmailButton.innerHTML = "SENT!!";
-    }
-    
+        function sendMessage() {
+            emailjs.send(service_id,template_id,template_params);
+
+            sendEmailButton.classList.remove('is-loading');        
+            sendEmailButton.classList.toggle('is-danger');
+            sendEmailButton.innerHTML = "SENT!!";
+        }
+        
+    });
+
+    //menu on mobile toggle
+    document.getElementById('menuToggleButton').addEventListener('click', function(){
+        document.getElementById('menuToggleButton').classList.toggle('is-active');
+    });
 });
