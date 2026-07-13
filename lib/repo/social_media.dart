b6fa12a53b225e32f75e2a6e8bebf01b4d94af3c
Josiah Thobejane <>
2026-07-13 13:13:46 +0200

    refactor(icons): change  icons

--- lib/repo/social_media.dart
+++ lib/repo/social_media.dart
@@ -1,5 +1,5 @@
-import 'package:eva_icons_flutter/eva_icons_flutter.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:website/model/social_media.dart';
 
 Color desktopViewIconColor = Colors.white;
@@ -10,24 +10,24 @@ class SocialMediaRepo {
   List<SocialMediaButtonModel> desktopButtons = [
     SocialMediaButtonModel(
       title: "LinkedIn",
-      icon: Icon(EvaIcons.linkedin, color: desktopViewIconColor),
+      icon: Icon(FontAwesomeIcons.linkedin, color: desktopViewIconColor),
       color: Color.fromRGBO(62, 142, 208, 1),
       redirectUrl: "https://linkedin.com/in/josiahthobejane"),
     SocialMediaButtonModel(
       title: "Github",
-      icon: Icon(EvaIcons.github, color: desktopViewIconColor),
+      icon: Icon(FontAwesomeIcons.github, color: desktopViewIconColor),
       color:Color.fromRGBO(20, 28, 58, 1),
       redirectUrl: "https://github.com/josiahthobejane"
     ),
       SocialMediaButtonModel(
       title: "Portfolio",
-      icon: Icon(EvaIcons.code, color: desktopViewIconColor),
+      icon: Icon(FontAwesomeIcons.code, color: desktopViewIconColor),
       color: Color.fromRGBO(195, 195, 195, 1),
       redirectUrl: "https://josiahthobejane.com/portfolio"
     ),
       SocialMediaButtonModel(
       title: "Twitter",
-      icon: Icon(EvaIcons.twitter, color: desktopViewIconColor),
+      icon: Icon(FontAwesomeIcons.twitter, color: desktopViewIconColor),
       color: Color.fromRGBO(62, 142, 208, 1),
       redirectUrl: "https://twitter.com/josiahthobejane"
     ),
@@ -36,24 +36,24 @@ class SocialMediaRepo {
     List<SocialMediaButtonModel> mobileButtons = [
     SocialMediaButtonModel(
       title: "LinkedIn",
-      icon: Icon(EvaIcons.linkedin, color: mobileViewIconColor),
+      icon: Icon(FontAwesomeIcons.linkedin, color: mobileViewIconColor),
       color: Color.fromRGBO(62, 142, 208, 1),
       redirectUrl: "https://linkedin.com/in/josiahthobejane"),
     SocialMediaButtonModel(
       title: "Github",
-      icon: Icon(EvaIcons.github, color: mobileViewIconColor),
+      icon: Icon(FontAwesomeIcons.github, color: mobileViewIconColor),
       color:Color.fromRGBO(20, 28, 58, 1),
       redirectUrl: "https://github.com/josiahthobejane"
     ),
       SocialMediaButtonModel(
       title: "Portfolio",
-      icon: Icon(EvaIcons.code, color: mobileViewIconColor,),
+      icon: Icon(FontAwesomeIcons.code, color: mobileViewIconColor,),
       color: Color.fromRGBO(195, 195, 195, 1),
       redirectUrl: "https://josiahthobejane.com/portfolio"
     ),
       SocialMediaButtonModel(
       title: "Twitter",
-      icon: Icon(EvaIcons.twitter, color: mobileViewIconColor,),
+      icon: Icon(FontAwesomeIcons.twitter, color: mobileViewIconColor,),
       color: Color.fromRGBO(62, 142, 208, 1),
       redirectUrl: "https://twitter.com/josiahthobejane"
     ),
